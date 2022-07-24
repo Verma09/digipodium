cb4dadf61cb03eebedd297bdc4549de7a6fee1c3
Verma09 <108970976+Verma09@users.noreply.github.com>
2022-07-24 12:08:56 +0530

    loop

---
+++ poiygon p1.py
@@ -0,0 +1,17 @@
+from turtle import *
+
+speed('slowest')
+pencolor('red')
+bgcolor('white')
+pensize(10)
+
+side = 6
+size = 100
+fillcolor('blue')
+begin_fill()
+for i in range(side):
+    fd(size)
+    lt(360/side)
+    
+end_fill()   
+mainloop()
---
+++ t3.py
@@ -0,0 +1,7 @@
+form turtle import*
+speed('slowest')
+pencolor('blue')
+bgcolor('white')
+forword(50)
+left(50)
+right(50)
